7d2b293297b6bf6ecaf562c348bd690b11375b21
Eduardo Brito <ebb2@cin.ufpe.br>
2018-07-05 22:44:48 +0000

    Software-06: Correcao em monitor.py

--- Software/monitor.py
+++ Software/monitor.py
@@ -2,7 +2,6 @@ from knotpy import *
 from credentials import *
 from SafeAndSound import *
 
-
 def had_changes(data = None):
 	new_data =  conn.getData(thing['uuid'], limit=1)[0]['data']['value']
 	return data != new_data
@@ -22,12 +21,15 @@ myThings = conn.getThings()
 
 data = None
 while True:
-	if had_changes(data[0]['data']['value']):
-		open_sensor = conn.getData(thing['uuid'], limit=1)[0]['data']['value']
-		if is_to_notify():
-			try:
-				device = device.models.Device.objects.get(bluetooth_id=open_sensor)
-				user = device.user
-				notify(device, user, not open_sensor)
-			except:
-				continue
+	myThings = conn.getThings()
+
+	for thing in myThings:
+		if had_changes(data):
+			data = conn.getData(thing['uuid'], limit=1)[0]['data']['value']
+			if is_to_notify():
+				try:
+					device = device.models.Device.objects.get(bluetooth_id=data)
+					user = device.user
+					notify(device, user, not data)
+				except:
+					continue
